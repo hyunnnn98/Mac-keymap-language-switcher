c49ce3093ca6781ce9b9ca4db5499ec492b5270c
SeungHyun Cho <c0305sh@naver.com>
2022-01-04 17:49:33 +0900

    Update : putting comments in code

--- init.lua
+++ init.lua
@@ -1,44 +1,55 @@
 local FRemap = require('foundation_remapping')
 local remapper = FRemap.new()
+
+-- custom key mapping --------------------------------------------------------------
 remapper:remap('capslock', 'f18')
 remapper:remap('rightcmd', 'f17')
+------------------------------------------------------------------------------------
 
--- 현재 사용중인 키맵보기
+-- Check the name of the language currently in use ---------------------------------
 -- hs.hotkey.bind({"cmd", "shift"}, "0", function ()
 -- hs.alert.show(hs.keycodes.currentSourceID())
 -- end)
+------------------------------------------------------------------------------------
 
-local caps_mode = hs.hotkey.modal.new()
+-- language mapping ----------------------------------------------------------------
 local inputEnglish = "com.apple.keylayout.ABC"
-local inputKorea = "com.apple.inputmethod.Korean.2SetKorean"
+local inputKorean = "com.apple.inputmethod.Korean.2SetKorean"
 local inputJapanese = "com.apple.inputmethod.Kotoeri.RomajiTyping.Japanese"
+------------------------------------------------------------------------------------
+
+-- alert style ---------------------------------------------------------------------
 local showSeconds = 0.5
 local cleanAlertStyle = {radius = 20, strokeColor = { white = 0, alpha = 0.3 }}
+------------------------------------------------------------------------------------
+
+local caps_mode = hs.hotkey.modal.new()
 
 local on_caps_mode = function()
     caps_mode:enter()
 end
 
--- 한영 변경 키
+-- Change from Korean to English ---------------------------------------------------
 local off_caps_mode = function()
 
     caps_mode:exit()
 
     local input_source = hs.keycodes.currentSourceID()
 
-    if input_source == inputKorea then
+    if input_source == inputKorean then
         hs.keycodes.currentSourceID(inputEnglish)
         language = '🇺🇸 English'
     elseif input_source == inputEnglish then
-        hs.keycodes.currentSourceID(inputKorea)
+        hs.keycodes.currentSourceID(inputKorean)
         language = '🇰🇷 Korean'
     end
 
     hs.alert.closeAll()
     hs.alert.show(language, cleanAlertStyle, showSeconds)
 end
+------------------------------------------------------------------------------------
 
--- 일영 변경 키
+-- Change from Japanese to English -------------------------------------------------
 local off_rightcmd_mode = function()
 
     caps_mode:exit()
@@ -56,8 +67,11 @@ local off_rightcmd_mode = function()
     hs.alert.closeAll()
     hs.alert.show(language, cleanAlertStyle, showSeconds)
 end
+------------------------------------------------------------------------------------
 
+-- Key binding ---------------------------------------------------------------------
 hs.hotkey.bind({}, 'f18', on_caps_mode, off_caps_mode)
 hs.hotkey.bind({}, 'f17', on_caps_mode, off_rightcmd_mode)
+------------------------------------------------------------------------------------
 
 remapper:register()
